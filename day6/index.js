ba499f06168d068d02dba1ea306e0cfe111ba2c6
nitinkmrp <nitinkmrp@gmail.com>
2025-09-30 15:38:25 +0530

    Add files via upload

---
+++ day6/index.js
@@ -0,0 +1,18 @@
+// alert("please ente name id name and age");
+// function transferButtont() {
+//  const a = document.getElementById("ID");
+// //  const b = document.getElementById("Name");
+// //  const c = document.getElementById("Age");
+
+//  const h = a.value;ugughggghghgc
+ 
+
+
+
+// // d=(a+b+c)/2;
+// alert(h);
+
+// }
+
+
+
